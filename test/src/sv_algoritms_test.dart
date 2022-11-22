7955fca16787dd3d372327166384caf34248e30f
Tomás Sasovsky <tomas@sportsvisio.com>
2022-11-21 21:34:41 -0300

    chore(algorithms): first commit

---
+++ test/src/sv_algoritms_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+// ignore_for_file: prefer_const_constructors
+import 'package:test/test.dart';
+import 'package:sv_algoritms/sv_algoritms.dart';
+
+void main() {
+  group('SvAlgoritms', () {
+    test('can be instantiated', () {
+      expect(SvAlgoritms(), isNotNull);
+    });
+  });
+}
