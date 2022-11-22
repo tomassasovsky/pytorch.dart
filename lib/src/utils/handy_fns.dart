da81efc243c364a35c5edc4df31825337f7d432e
Tomás Sasovsky <tomas@sportsvisio.com>
2022-11-22 11:36:23 -0300

    feat: frame from video file function

--- lib/src/utils/handy_fns.dart
+++ lib/src/utils/handy_fns.dart
@@ -5,10 +5,63 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+import 'dart:io';
+
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:flutter/services.dart';
 import 'package:opencv_4/factory/pathfrom.dart';
 import 'package:opencv_4/opencv_4.dart';
 import 'package:scidart/numdart.dart';
+
+/// Reads an image from a movie or image file
+///
+/// Required argument:
+/// inputFile -- string, name of file. Must be a full url path, not relative.
+///
+/// Optional argument:
+/// frame -- int, frame number to read (default None, reads first frame)
+///
+/// Returns image array converted to RGB, 0 to 255
+Future<Uint8List> getImageFrame({
+  int frame = 1,
+  String? inputFile,
+  String? outputPath,
+}) async {
+  final i = frame;
+  final file = File('${outputPath ?? Directory.systemTemp.path}/frame$i.jpg');
+  if (file.existsSync()) {
+    file.deleteSync();
+  }
+
+  final result = await FFmpegKit.execute(
+    '-accurate_seek -ss `echo $i*60.0 | bc` -i $inputFile '
+    '-frames:v 1 frame$i.jpg',
+  );
+
+  final returnCode = await result.getReturnCode();
+  if (returnCode?.isValueSuccess() ?? false) {
+    final image = await Cv2.cvtColor(
+      pathString: file.path,
+      pathFrom: CVPathFrom.URL,
+      outputType: Cv2.COLOR_BGR2RGB,
+    );
+
+    if (image != null) {
+      return image;
+    }
+
+    throw PlatformException(
+      code: 'getImageFrame',
+      message: 'Failed to read image frame',
+    );
+  }
+
+  throw PlatformException(
+    code: returnCode.toString(),
+    message: 'Failed to get frame $i',
+    stacktrace: await result.getFailStackTrace(),
+  );
+}
 
 /// Return a sharpened version of the image, using an unsharp mask.
 Future<Uint8List> unsharpMask({
